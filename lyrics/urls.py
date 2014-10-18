db9b20312e99fcf30f9213bb93230d7ea961d3de
Evgenii Nikolaev <evgeny.albertovich@gmail.com>
2014-10-18 23:47:40 +0400

    added admin interface

--- lyrics/urls.py
+++ lyrics/urls.py
@@ -1,5 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
-     url(r'^$', 'lyrics.views.index', name='index'),
+    url(r'^$', 'lyrics.views.index', name='index'),
+    url(r'^admin/', include(admin.site.urls)),
 )
