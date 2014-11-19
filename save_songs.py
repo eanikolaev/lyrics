d2efc3316c8237df3de24242db688b0a0518bb26
Evgenii Nikolaev <evgeny.albertovich@gmail.com>
2014-11-19 20:35:50 +0400

    added bulk create

--- save_songs.py
+++ save_songs.py
@@ -6,13 +6,21 @@ import csv
 
 # set input file name here
 INPUT = "collect_data/songs.csv"
-
+MAX_BULK = 1000
 
 if __name__=='__main__':
     csvfile = open(INPUT, 'rb')
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for (aid, artist, title, duration, url, lyrics) in reader:
-        s = Song(aid=aid, artist=artist, title=title, duration=duration, url=url, lyrics=lyrics)
-        s.save()
+    bulk = []
+    i = 0    
+    for (aid, artist, title, duration, lyrics, url) in reader:
+        if i<MAX_BULK:
+            bulk.append(Song(aid=aid, artist=artist, title=title, duration=duration, url=url, lyrics=lyrics))
+            i += 1
+
+        else:
+            Song.objects.bulk_create(bulk)
+            bulk = []
+            i = 0
         
 
